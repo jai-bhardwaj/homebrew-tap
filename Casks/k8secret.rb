86626ab63ecbd7646d0d2a8a0d914518e33d5403
Jai Bhardwaj <0987sujals@gmail.com>
2026-08-19 11:36:53 +0530

    k8secret 0.6.20

--- Casks/k8secret.rb
+++ Casks/k8secret.rb
@@ -1,6 +1,6 @@
 cask "k8secret" do
-  version "0.6.19"
-  sha256 "52ec4fbba338ed4377c0fd39b26a226284c3dee0b09997b1d4d26914e20c3d3b"
+  version "0.6.20"
+  sha256 "315fc76505dc20d3ee08152e93a1d22033132cff1fab081c45437faf754f625e"
 
   url "https://github.com/jai-bhardwaj/k8secret/releases/download/v#{version}/K8Secret-#{version}.dmg"
   name "K8Secret"
